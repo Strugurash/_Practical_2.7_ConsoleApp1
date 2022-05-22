b84a510c2822855945924fb17d698633c27cdbfb
User <genik-vlad@mail.ru>
2022-05-22 22:33:34 +0300

    Добавьте файлы проекта.

---
+++ _Practical_2.7_ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _Practical_2._7_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Fullname = "Игнатов Геннадий Владимирович";
+            int age = 44;
+            string email = "strugurash@gmail.com";
+            double PointsInf = 4.2; // средний балл информатика
+            double PointsMath = 4.5; // средний балл математика
+            double Pointsphy = 3.7; // средний балл физика
+            string pattern = " ФИО : {0}\n Возраст : {1}\n email : {2}\n Информатика : {3}\n Математика : {4}\n Физика : {5}";
+            Console.WriteLine(pattern, Fullname, age, email, PointsInf, PointsMath, Pointsphy);
+            double Tpoints = PointsInf + PointsMath + Pointsphy;
+            double GPA = Tpoints / 3;
+            string newpattern = "Сумма баллов : {0}\n Средний балл : {1}";
+            Console.ReadKey();
+            Console.WriteLine(newpattern, Tpoints, GPA);
+        }
+    }
+}
---
+++ _Practical_2.7_ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("_Practical_2.7_ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_Practical_2.7_ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ff57d4ba-bebd-4669-a727-1a8c0e293a88")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
